6ca55485e0b2812e5adead7604b7b9b96a64e108
Quang Nguyen <quang.sunlight@gmail.com>
2022-04-22 15:04:47 +0400

    Setup deploy to firebase

--- back-end/index.ts
+++ back-end/index.ts
@@ -4,6 +4,8 @@ import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+console.log(process.env.NODE_ENV);
+
 // built-in middleware to handle urlencoded data
 // in other words, form data:
 // 'content-type: application/x-www-form-urlencoded'
@@ -13,10 +15,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve static files
-app.use('/', express.static(path.join(__dirname, '..', 'front-end', 'build')));
+let fePath = path.join(__dirname, '..', 'front-end', 'build');
+console.log(process.env.NODE_ENV, 'production', process.env.NODE_ENV == 'production');
+if (process.env.NODE_ENV == 'production') {
+  fePath = path.join(__dirname, '..', '..', 'front-end', 'build');
+  console.log('prod', fePath);
+}
+app.use('/', express.static(fePath));
 const router = express.Router();
 router.get('^/$|/index(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'front-end', 'build', 'index.html'));
+  res.sendFile(path.join(fePath, 'index.html'));
 });
 app.use('/', router);
 
