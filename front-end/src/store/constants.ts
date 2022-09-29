f22986ad35e139f42e3adc3e887a91e426dd0ed3
Quang Nguyen <quang.sunlight@gmail.com>
2022-09-29 17:19:03 +0400

    chore: console log the process.env

--- front-end/src/store/constants.ts
+++ front-end/src/store/constants.ts
@@ -1,2 +1,4 @@
+console.log(process.env);
+
 export const sidebarWidth = 240;
-export const API_ENDPOINT = 'http://localhost:3500';
+export const API_ENDPOINT = process.env.API_ENDPOINT ?? 'http://localhost:3500';
