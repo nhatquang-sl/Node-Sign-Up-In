d22fcd51d3c538bbf451019d9763e22ee7848c54
Quang Nguyen <quang.sunlight@gmail.com>
2022-10-06 05:30:56 +0400

    feat: testing deploy heroku

--- front-end/src/store/constants.ts
+++ front-end/src/store/constants.ts
@@ -1,5 +1,4 @@
 console.log(process.env);
 
 export const sidebarWidth = 240;
-export const API_ENDPOINT =
-  process.env.REACT_APP_ENV === 'production' ? 'api' : 'http://localhost:3500';
+export const API_ENDPOINT = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3500';
