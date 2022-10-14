062ec677b80e96c1ce0c82f473b0f7323554f0fc
Quang Nguyen <quang.sunlight@gmail.com>
2022-10-14 06:49:14 +0400

    feat(bnb): connect bnb socket to get market price

--- front-end/src/shared/utilities/math.ts
+++ front-end/src/shared/utilities/math.ts
@@ -1,6 +1,8 @@
 // Number.EPSILON in case round 1.005 => 1.01
 const round2Dec = (num: number) => Math.round((num + Number.EPSILON) * 100) / 100;
 
+const round3Dec = (num: number) => Math.round((num + Number.EPSILON) * 1000) / 1000;
+
 const round4Dec = (num: number) => Math.round((num + Number.EPSILON) * 10000) / 10000;
 
-export { round2Dec, round4Dec };
+export { round2Dec, round3Dec, round4Dec };
