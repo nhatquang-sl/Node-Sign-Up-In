7633254bf39111d245717bc8e8c56483ba9d40da
Quang Nguyen <quang.sunlight@gmail.com>
2022-10-14 10:41:09 +0400

    feat(bnb): get future orders

--- front-end/src/shared/bnb/service.ts
+++ front-end/src/shared/bnb/service.ts
@@ -1,34 +1,50 @@
 import axios, { AxiosInstance } from 'axios';
+import CryptoJS from 'crypto-js';
 import { Kline } from './dto';
 
 export class BnbService {
   fapi: AxiosInstance;
-  api: AxiosInstance;
-  constructor() {
+  apiKey: string;
+  secretKey: string;
+
+  constructor(apiKey: string = '', secretKey: string = '') {
+    this.apiKey = apiKey;
+    this.secretKey = secretKey;
     this.fapi = axios.create({
       baseURL: 'https://fapi.binance.com',
-    });
-
-    this.api = axios.create({
-      baseURL: 'https://api.binance.com',
+      headers: { 'X-MBX-APIKEY': this.apiKey },
     });
   }
 
   getServerTime = async () => {
-    var res = await this.api.get('/api/v3/time');
+    var res = await this.fapi.get('/fapi/v1/time');
     return res.data['serverTime'];
   };
 
   getKlines = async (symbol: string, interval: string, limit: number = 1000) => {
     const klines: Kline[] = [];
-    const res = await this.api.get(
-      `/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`
+    const res = await this.fapi.get(
+      `/fapi/v1/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`
     );
 
     for (const kline of res.data) {
       klines.push(new Kline(kline));
     }
     return klines;
+  };
+
+  getListenKey = async () => {
+    var res = await this.fapi.post('/fapi/v1/listenKey');
+
+    return res.data['listenKey'];
+  };
+
+  getAllOrders = async (symbol: string, timestamp: number) => {
+    const query = `symbol=${symbol}&timestamp=${timestamp}`;
+    const signature = CryptoJS.HmacSHA256(query, this.secretKey).toString(CryptoJS.enc.Hex);
+
+    var res = await this.fapi.get(`/fapi/v1/allOrders?${query}&signature=${signature}`);
+    return res.data;
   };
 }
 
