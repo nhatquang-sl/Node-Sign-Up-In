32d0ddba2bfda22c7b2465e6ca292d9cf8fb615d
Quang Nguyen <quang.sunlight@gmail.com>
2022-11-09 15:08:13 +0400

    refactor: profile page

--- front-end/src/store/auth/actions.ts
+++ front-end/src/store/auth/actions.ts
@@ -1,41 +1,6 @@
-import { UserLoginDto, UserRegisterDto, UserAuthDto } from 'shared/user/dto';
 import { AUTH_TYPE } from './types';
 
 import { apiService } from '../services';
-
-const login = (request: UserLoginDto) => ({
-  type: AUTH_TYPE.LOGIN,
-  payload: apiService.post(`auth/login`, request, { withCredentials: true }),
-});
-
-const register = (request: UserRegisterDto) => ({
-  type: AUTH_TYPE.REGISTER,
-  payload: apiService.post(`auth/register`, request),
-});
-
-const registerConfirm = (activationCode: string) => ({
-  type: AUTH_TYPE.REGISTER_CONFIRM,
-  payload: apiService.get(`auth/activate/${activationCode}`),
-});
-
-const logOut = () => ({
-  type: AUTH_TYPE.LOG_OUT,
-});
-
-const updateAuth = (userAuth: UserAuthDto) => ({
-  type: AUTH_TYPE.UPDATE,
-  payload: userAuth,
-});
-
-const sendActivateLink = () => ({
-  type: AUTH_TYPE.SEND_ACTIVATE_LINK,
-  payload: apiService.post(`auth/send-activation-email`),
-});
-
-const getProfile = () => ({
-  type: AUTH_TYPE.GET_USER_PROFILE,
-  payload: apiService.get(`auth/profile`),
-});
 
 const getLastDateResetPassword = (emailAddress: string) => ({
   type: AUTH_TYPE.GET_RESET_PASSWORD_LAST_DATE,
@@ -52,15 +17,4 @@ const setNewPassword = (token: string, password: string) => ({
   payload: apiService.post(`auth/reset-password/set-new`, { token, password }),
 });
 
-export {
-  login,
-  register,
-  registerConfirm,
-  logOut,
-  updateAuth,
-  sendActivateLink,
-  getProfile,
-  getLastDateResetPassword,
-  getSendEmailResetPassword,
-  setNewPassword,
-};
+export { getLastDateResetPassword, getSendEmailResetPassword, setNewPassword };
